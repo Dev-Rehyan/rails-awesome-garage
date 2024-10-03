cb20fe5a72aeda907cbb2993f5da545aedd36be2
Rehyan Rhoden <rehyan92@hotmail.co.uk>
2024-10-03 11:28:29 +0100

    added validations

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,11 @@
 class Car < ApplicationRecord
   belongs_to :owner
+
+  has many :favorites
+  has_many :reviews
+
+  validates :brand, presence: true
+  validates :model, presence: true
+  validates :year, presence: true, numericality: { only_integer: true }
+  validates :fuel, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :car
+
+  validates :comment, presence: true, length: { minimum: 6, too_short: "must have at least 6 characters" }
+  validates :rating, presence: true, numericality: { only_integer: true }
 end
